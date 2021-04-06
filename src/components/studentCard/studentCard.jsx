28921c91aa58e7c33b0e61af042b727ccb6e4c68
jperezviloria <protobot.dev@gmail.com>
2021-04-05 23:30:11 -0300

    initialize functionalities to upload user image

--- src/components/studentCard/studentCard.jsx
+++ src/components/studentCard/studentCard.jsx
@@ -1,12 +1,20 @@
 import React from "react"
 import "./studentCard.css"
+import axios from "axios"
+import {useForm} from "react-hook-form"
+
 
 const StudentCard = (props) =>{
+
+    
+
+
     return (
         <div className="studentCard">
             <img src= "https://cde.laprensa.e3.pe/ima/0/0/1/8/3/183593.jpg"alt=""/>
             <p>{props.emailUser}</p>
             <a href="">see more</a>
+            
         </div>
     )
 }
--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,5 +1,7 @@
 import React, {useContext, useEffect, useState} from "react"
 import UserAuthContext from "../../context/userAuth/userAuthContext"
+import axios from "axios"
+import {useForm} from "react-hook-form"
 
 import "./UserCard.css"
 
@@ -19,6 +21,11 @@ const ProfileCard = () =>{
         
     }
 
+    const {register, handleSubmit} = useForm()
+
+    const onsubmitImage = async(data) =>{
+        const image = await axios.post(`http://localhost:3001/publicuser/images/add`, data)
+    }
 
     useEffect(() =>{
         checkAuth()
@@ -37,6 +44,13 @@ const ProfileCard = () =>{
                 : <div>
                     <img src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2021/03/avatar-scaled.jpeg?fit=1200%2C675&ssl=1" alt=""/>
         <h3>user: {localStorage.getItem("user")}</h3>
+        <form onSubmit={handleSubmit(onsubmitImage)}>
+                <input 
+                type="file" 
+                name="image"
+                ref={register()}/>
+                <button type="submit">send image</button>
+            </form>
             </div>
             }
             
